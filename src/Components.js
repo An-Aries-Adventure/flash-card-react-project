e3a87ee7cbb1e7928644e55632c495d89017fc98
unknown <kylespinaio@hotmail.com>
2020-07-23 21:54:08 -0400

    outlined class and functions per user stories and wireframe needed for app to excecute.

---
+++ src/Components.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import Axios from 'axios';
+
+
+
+class AddNewCard{}() =>{
+
+};
+
+
+
+
+
+function startUpApp(){
+
+};
+
+
+
+
+function chooseTab(){
+
+
+};
+
+
+
+
+function currentCardNumber(){
+
+};
+
+
+
+
+
+function editCurrentCard(){
+
+
+};
+
+
+
+
+
+function deleteCurrentCard(){
+
+
+};
+
+
+
+
+function flipCardOver(){
+
+
+};
+
+
+function advanceCard(){
+
+
+};
+
+
+
+function preivousCard(){
+
+
+};
